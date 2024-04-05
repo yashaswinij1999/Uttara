016058d17b0b47baa2f3651f0053e47e393fca11
Yashaswini J <yashaswinij1999>
2024-04-05 11:05:25 +0530

    refactored

--- Attempt 1/Functions and Strings/revWords.js
+++ Attempt 1/Functions and Strings/revWords.js
@@ -2,14 +2,19 @@ function revWords(str) {
   split = str.split(" ");
   console.log(split);
 
-  let string = "";
-  for (let i = 0; i < split.length; i++) {
-    let word = split[i];
+  // let string = "";
+  // for (let i = 0; i < split.length; i++) {
+  //   let word = split[i];
 
-    for (let j = word.length - 1; j >= 0; j--) {
-      string += word[j];
-      console.log(string);
-    }
-  }
+  //   for (let j = word.length - 1; j >= 0; j--) {
+  //     string += word[j];
+  //     console.log(string);
+  //   }
+  // }
+
+  const sentence = split.map((el) => {
+    return el.split("").reverse().join(" ");
+  });
+  console.log(sentence);
 }
 revWords("We love JS");
