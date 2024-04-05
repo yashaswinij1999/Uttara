8ee4d59afb27abbb3268c4094711a95f126e8140
Yashaswini J <yashaswinij1999>
2024-04-05 10:59:40 +0530

    using split to seperate space in string

--- Attempt 1/Functions and Strings/revWords.js
+++ Attempt 1/Functions and Strings/revWords.js
@@ -1,4 +1,15 @@
-function reverseWords(str) {
+function revWords(str) {
   split = str.split(" ");
   console.log(split);
+
+  let string = "";
+  for (let i = 0; i < split.length; i++) {
+    let word = split[i];
+
+    for (let j = word.length - 1; j >= 0; j--) {
+      string += word[j];
+      console.log(string);
+    }
+  }
 }
+revWords("We love JS");
