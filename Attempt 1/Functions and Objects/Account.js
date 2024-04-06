f89ba39b397cf7ea38cc7a226d2128cc16c9af89
Yashaswini J <yashaswinij1999>
2024-04-06 10:40:30 +0530

    refactored account object

--- Attempt 1/Functions and Objects/Account.js
+++ Attempt 1/Functions and Objects/Account.js
@@ -6,13 +6,36 @@ class Account {
   }
 
   deposit(amt) {
-    this.bal = this.bal + amt;
-    return this.bal;
+    if (this.bal < 0) {
+      console.log("bal is lessthan zero");
+      return;
+    } else if (amt < 0) {
+      console.log("amt is lessthan zero");
+      return;
+    } else if (this.bal > 0 && amt > 0) {
+      this.bal = this.bal + amt;
+      console.log(`balance after depositing amount ${amt} = ${this.bal}`);
+    }
   }
 
   withdraw(amt) {
-    this.bal = this.bal - amt;
-    return this.bal;
+    if (bal < 0) {
+      console.log("bal is lessthan zero");
+      return;
+    }
+    if (amt < 0) {
+      console.log("the amount is lessthan zero");
+      return;
+    }
+    if (amt > this.bal) {
+      console.log("the amount entered is greater than balance");
+    }
+    if (amt < this.bal && this.bal > 0 && amt > 0) {
+      this.bal = this.bal - amt;
+      console.log(
+        `the bal after withdrawing amt ${amt} is equal to ${this.bal} `
+      );
+    }
   }
 }
 
@@ -21,4 +44,7 @@ console.log(rama["accNumber"]);
 console.log(rama["accHolderName"]);
 bal = rama.deposit(10);
 bal = rama.withdraw(16);
-console.log(bal);
+
+sita = new Account("96584623", "sita", 3);
+sita.withdraw(2);
+sita.deposit(9);
