33dcd602153949e82d822e6eb62541e37102da89
Yashaswini J <yashaswinij1999>
2024-04-07 13:25:04 +0530

    returning duplicate elements after merging an array

--- Attempt 1/FunctionalArray/checkDuplicates.js
+++ Attempt 1/FunctionalArray/checkDuplicates.js
@@ -1,7 +1,14 @@
 function checkDuplicates(arr1, arr2) {
   let arr3 = [...arr1, ...arr2];
+
+  const duplicates = arr3.filter((el, index, arr3) => {
+    return arr3.indexOf(el) === index;
+  });
+
+  return duplicates;
 }
 
 let arr1 = [1, 5, 3];
 let arr2 = [4, 5, 6];
-checkDuplicates(arr1, arr2);
+
+console.log(checkDuplicates(arr1, arr2));
