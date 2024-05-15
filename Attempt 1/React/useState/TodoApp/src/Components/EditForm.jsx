10e9284b3d8298a631b1495ce1f72275c509bbce
Yashaswini J <yashaswinij1999>
2024-05-15 18:36:34 +0530

    paused with edit functionality

---
+++ Attempt 1/React/useState/TodoApp/src/Components/EditForm.jsx
@@ -0,0 +1,7 @@
+export default function EditForm() {
+  return (
+    <>
+      <input className="p-2 m-2 rounded-md " type="text" />
+    </>
+  );
+}
--- Attempt 1/React/useState/TodoApp/src/Components/TodoApp.jsx
+++ Attempt 1/React/useState/TodoApp/src/Components/TodoApp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Form from "./Form";
+import EditForm from "./EditForm";
 
 function TodoApp() {
   const initialData = [
@@ -9,23 +10,50 @@ function TodoApp() {
   ];
 
   const [data, setData] = useState(initialData);
+  const [isEdit, setEdit] = useState(false);
 
   const addTodo = function (todo) {
     const newTodo = { id: uuidv4(), task: todo };
     setData([...data, newTodo]);
   };
 
+  const delTodo = function (id) {
+    const filtered = data.filter((el) => el.id !== id);
+    setData(filtered);
+  };
+
   return (
     <>
-      <div className=" w-6/12 text-center m-auto">
+      <div className=" w-6/12  m-auto">
         <Form addTodo={addTodo} />
-        <ul>
-          {data.map((el) => (
-            <li className=" p-1 m-2 rounded-md  bg-slate-100" key={el.id}>
-              {el.task}
-            </li>
-          ))}
-        </ul>
+        {isEdit ? (
+          <EditForm />
+        ) : (
+          <ul>
+            {data.map((el) => (
+              <li
+                className=" p-1 m-2 rounded-md bg-slate-100 flex justify-around items-center"
+                key={el.id}
+              >
+                {el.task}
+                <div className="flex gap-2">
+                  <button
+                    className="w-fit p-1 bg-violet-400 rounded-lg"
+                    onClick={() => setEdit(true)}
+                  >
+                    edit
+                  </button>
+                  <button
+                    className="w-fit p-1 bg-red-400 rounded-lg"
+                    onClick={() => delTodo(el.id)}
+                  >
+                    delete
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </>
   );
