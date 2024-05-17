332b14a5110d6403266518352ec7f60ea32431dc
Yashaswini J <yashaswinij1999>
2024-05-17 17:46:04 +0530

    initial commit and created navabr component

---
+++ Attempt 1/React/Router/ShoppingCart/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
---
+++ Attempt 1/React/Router/ShoppingCart/src/Components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <nav className="">
+        <div>Home</div>
+        <div>Contact</div>
+        <div></div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
