cbd6b5f1e88e391cc1c234e02152b0a823b3955b
Yashaswini J <yashaswinij1999>
2024-06-22 14:33:08 +0530

    styled the component

--- Attempt 1/FullStack/ShoppingCart-FullStack/frontend/src/Components/ProductList.jsx
+++ Attempt 1/FullStack/ShoppingCart-FullStack/frontend/src/Components/ProductList.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Product from "./Product";
 
 function ProductList() {
   const [data, setData] = useState([]);
@@ -11,13 +12,25 @@ function ProductList() {
   async function getData() {
     try {
       const response = await axios.get("http://localhost:3000/products/");
+      console.log(response.data);
       setData(response.data);
     } catch (error) {
       console.log(error);
     }
   }
 
-  return <div>ProductList</div>;
+  return (
+    <div className="min-h-screen m-2 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4">
+      {data.map((el) => (
+        <Product
+          key={el._id}
+          title={el.title}
+          price={el.price}
+          image={el.image}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ProductList;
